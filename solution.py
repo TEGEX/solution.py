72268bcc634b04becf7f557bce95d0b9d2edd103
Ge <74381345+TEGEX@users.noreply.github.com>
2021-02-22 20:20:24 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -30,10 +30,11 @@ def webServer(port=13331):
 
            #Send the content of the requested file to the client
            for i in range(0, len(outputdata)):
-           connectionSocket.send(outputdata[i].encode())
+               connectionSocket.send(outputdata[i].encode())
+
            connectionSocket.send("\r\n".encode())
            connectionSocket.close()
-           except IOError:
+       except IOError:
            #Send response message for file not found (404)
            #Fill in start
            connectionSocket.send(bytes('HTTP/1.1 404 Not Found\r\n\r\n','UTF-8'))
