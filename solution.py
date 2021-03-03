7f244f6b594a2722497cfda3aa3b9a8274d0207d
Ge <74381345+TEGEX@users.noreply.github.com>
2021-03-02 20:03:54 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,23 +14,13 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     clientSocket.connect((mailserver, port))
     # Fill in end
 
-    recv = clientSocket.recv(1024).decode()
-    #print(recv)
-    #if recv[:3] != '220':
-        #print('220 reply not received from server.')
-
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
-    #recv1 = clientSocket.recv(1024).decode()
+    recv1 = clientSocket.recv(1024).decode()
     #print(recv1)
     #if recv1[:3] != '250':
-       # print('250 reply not received from server.')
-    
-    # Send message data.
-    # Fill in start
-    clientSocket.send(msg.encode())
-    # Fill in end    
+       # print('250 reply not received from server.')   
     
     # Send MAIL FROM command and print server response
     # Fill in start
