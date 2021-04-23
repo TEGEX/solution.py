5919e7ed27501d5893c4d6cdf1f9d55c5c543166
Ge <74381345+TEGEX@users.noreply.github.com>
2021-04-23 13:35:05 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -48,15 +48,33 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         recPacket, addr = mySocket.recvfrom(1024)
 
         # Fill in start
-        ICMPHeader = recPacket[20:28]
-        icmpType, code, checksum, packetID, sequence = struct.unpack("bbHHh", ICMPHeader)
-        IPheader = recPacket[8:9]
-        TTL = struct.unpack("B", IPheader)# Print ping results
-        if packetID == ID:
-            byte = struct.calcsize("d")
-            timeSent = struct.unpack("d", recPacket[28:28 + byte])[0]
-        return "Reply from %s: bytes=%d time=%f5ms TTL=%d" % (destAddr, len(recPacket), (timeReceived - timeSent)*1000, TTL[0])
-
+        # Fetch the ICMP header from the IP packet
+        icmpHeader = recPacket[20:28]
+        # print('icmpHeader= ', icmpHeader)
+        struct_format = "bbHHh"
+        unpacked_data = struct.unpack(struct_format, icmpHeader)
+        # print("unpacked_data= ", unpacked_data)
+        type, code, checksum, temp_id, seq = struct.unpack('bbHHh', icmpHeader)
+        # print('type=', type, 'code=', code, 'checksum=', checksum, 'temp_id=', temp_id, 'seq=', seq)
+        if type != 0:
+            return 'expected type=0, but got {}'.format(type)
+        if code != 0:
+            return 'expected code=0, but go {}'.format(code)
+        if ID != temp_id:
+            return 'expected id={}, but got {}'.format(ID, id)
+        send_time, = struct.unpack('d', recPacket[28:])
+        # print('send_time=', send_time, 'timeReceived=', timeReceived)
+        rtt = (timeReceived - send_time) * 1000
+        ip_header = struct.unpack('!BBHHHBBH4s4s', recPacket[:20])
+        # print('ip_header = ', ip_header)
+        ttl = ip_header[5]
+        saddr = socket.inet_ntoa(ip_header[8])
+        length = len(recPacket) - 20
+        timeLeft = timeLeft - howLongInSelect
+        if timeLeft <= 0:
+            return "Request timed out."
+        # return 'Reply from {}: bytes={} time={:.7f}ms TTL={}'.format(saddr, length, rtt, ttl)
+        return (saddr, length, rtt, ttl)
         # Fill in end
         timeLeft = timeLeft - howLongInSelect
         if timeLeft <= 0:
