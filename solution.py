d4aabdbd286d9309f3ee79bee123e11732a02e36
Ge <74381345+TEGEX@users.noreply.github.com>
2021-04-23 13:55:20 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -48,33 +48,27 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         recPacket, addr = mySocket.recvfrom(1024)
 
         # Fill in start
-        # Fetch the ICMP header from the IP packet
-        icmpHeader = recPacket[20:28]
-        # print('icmpHeader= ', icmpHeader)
-        struct_format = "bbHHh"
-        unpacked_data = struct.unpack(struct_format, icmpHeader)
-        # print("unpacked_data= ", unpacked_data)
-        type, code, checksum, temp_id, seq = struct.unpack('bbHHh', icmpHeader)
-        # print('type=', type, 'code=', code, 'checksum=', checksum, 'temp_id=', temp_id, 'seq=', seq)
+        type, code, checksum, id, seq = struct.unpack('bbHHh', recPacket[20:28])
         if type != 0:
             return 'expected type=0, but got {}'.format(type)
         if code != 0:
-            return 'expected code=0, but go {}'.format(code)
-        if ID != temp_id:
+            return 'expected code=0, but got {}'.format(code)
+        if ID != id:
             return 'expected id={}, but got {}'.format(ID, id)
-        send_time, = struct.unpack('d', recPacket[28:])
-        # print('send_time=', send_time, 'timeReceived=', timeReceived)
+        send_time,  = struct.unpack('d', recPacket[28:])
+        
         rtt = (timeReceived - send_time) * 1000
-        ip_header = struct.unpack('!BBHHHBBH4s4s', recPacket[:20])
-        # print('ip_header = ', ip_header)
+        rtt_cnt += 1
+        rtt_sum += rtt
+        rtt_min = min(rtt_min, rtt)
+        rtt_max = max(rtt_max, rtt)
+
+        ip_header = struct.unpack('!BBHHHBBH4s4s' , recPacket[:20])
         ttl = ip_header[5]
         saddr = socket.inet_ntoa(ip_header[8])
         length = len(recPacket) - 20
-        timeLeft = timeLeft - howLongInSelect
-        if timeLeft <= 0:
-            return "Request timed out."
-        # return 'Reply from {}: bytes={} time={:.7f}ms TTL={}'.format(saddr, length, rtt, ttl)
-        return (saddr, length, rtt, ttl)
+
+        return '{} bytes from {}: icmp_seq={} ttl={} time={:.3f} ms'.format(length, saddr, seq, ttl, rtt)
         # Fill in end
         timeLeft = timeLeft - howLongInSelect
         if timeLeft <= 0:
